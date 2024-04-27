bae68a1b2b08889888ca2c8c7e95ea15fef875e3
Rodrigo Arguello Serrano <rodrigo19662001@gmail.com>
2024-04-26 19:21:16 -0700

    Adding file for variables and memory

--- Rectangle.py
+++ Rectangle.py
@@ -1,6 +1,69 @@
 class Rectangle:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, width, height) -> None:
+        self.width = width
+        self.height = height
+        
+    @property
+    def width(self):
+        return self._width
+        
+    @width.setter
+    def width(self, width):
+        if width <= 0:
+            raise ValueError(f'Width cannot be negative')
+        else:
+            self._width = width
+            
+    @property
+    def height(self):
+        return self._height
+        
+    @height.setter
+    def height(self, height):
+        if height <= 0:
+            raise ValueError(f'Height cannot be negative')
+        else:
+            self._height = height
+            
+
+    def area(self):
+        return self.width * self.height
+        
+    def perimeter(self):
+        return  2 * (self.width + self.height)
+
+    def __str__(self):
+        return f'Rectangle: width = {self.width}, height = {self.height}'
+
+    def __repr__(self):
+        return f'Rectangle({self.width}, {self.height})'
+
+    def __eq__(self, other: object) -> bool:
+        if isinstance(other, Rectangle):
+            return self.width == other.width and self.height == other.height
+        else:
+            return False
+
+    def __lt__(self, other: object) -> bool:
+        if isinstance(other, Rectangle):
+            return self.area() < other.area()
+        else:
+            return NotImplemented
 
 if __name__ == "__main__":
-    pass
+    r1 = Rectangle(10,20)
+    r2 = Rectangle(10,20)
+    r3 = Rectangle(10,20)
+    r4 = Rectangle(100,200)
+    print(f'The area of a Rectangle with width {r1.width} and height {r1.height} is {r1.area()} ')
+    print(f'The Perimeter of a Rectangle with width {r1.width} and height {r1.height} is {r1.perimeter()} ') 
+    print(f'This is r1 {r1} ')
+    print(f'Is r1 == r2? {r1 == r2}')
+    print(f'Is r1 == 100? {r1 == 100}')
+    
+
+    
+	
+
+
+
---
+++ variables_and_memory.py
@@ -0,0 +1,40 @@
+# The id function returns the memory address 
+#Exmaple
+
+import ctypes
+import sys
+
+class CountReference:
+    def __init__(self, address):
+        self.address = address
+    
+    @property
+    def address(self):
+        """The address property."""
+        return self._address
+
+    @address.setter
+    def address(self, value: int):
+        self._address = ctypes.c_long.from_address(value).value
+
+
+a = 10
+print(f'This is the address of a: {hex(id(a))}')
+
+a = [1,2,3]
+
+print(f'a memory address as int {id(a)}')
+
+print(f'Get referece count using getrefcount: {sys.getrefcount(a)}')
+
+a_mem = CountReference(id(a))
+
+print(f'Count referece using ctypes {a_mem.address}')
+
+# Lets assign b
+b = a 
+print(f' a memory address is the same as b {id(a) == id(b)}')
+print(f"Let's get the reference count of a or b {CountReference(id(a)).address}")
+
+
+
